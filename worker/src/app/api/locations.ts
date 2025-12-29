c6a9aa8c4587720aef203e4de50abe81904fe40d
torounit <mail@torounit.com>
2025-12-30 03:16:21 +0900

    refactor: use chain

--- worker/src/app/api/locations.ts
+++ worker/src/app/api/locations.ts
@@ -9,68 +9,66 @@ import { z } from "zod";
 import * as schema from "../../db/schema";
 
 const locations = new Hono<{ Bindings: CloudflareBindings }>();
-locations.use(
-  "*",
-  basicAuth({
-    verifyUser: (
-      username,
-      password,
-      c: Context<{ Bindings: CloudflareBindings }>,
-    ) => {
-      return (
-        username === c.env.BASIC_AUTH_USERNAME &&
-        password === c.env.BASIC_AUTH_PASSWORD
-      );
-    },
-  }),
-);
-
-locations.post(
-  "/register",
-  zValidator(
-    "json",
-    z.object({
-      name: z.string(),
+locations
+  .use(
+    "*",
+    basicAuth({
+      verifyUser: (
+        username,
+        password,
+        c: Context<{ Bindings: CloudflareBindings }>,
+      ) => {
+        return (
+          username === c.env.BASIC_AUTH_USERNAME &&
+          password === c.env.BASIC_AUTH_PASSWORD
+        );
+      },
     }),
-  ),
-  async (c) => {
-    const data = c.req.valid("json");
+  )
+  .get("/", async (c) => {
     const db = drizzle(c.env.DB, { schema });
-    const existingLocation = await db.query.locations.findFirst({
-      where: eq(schema.locations.name, data.name),
-    });
+    const allLocations = await db.query.locations.findMany();
+    return c.json(allLocations);
+  })
+  .post(
+    "/register",
+    zValidator(
+      "json",
+      z.object({
+        name: z.string(),
+      }),
+    ),
+    async (c) => {
+      const data = c.req.valid("json");
+      const db = drizzle(c.env.DB, { schema });
+      const existingLocation = await db.query.locations.findFirst({
+        where: eq(schema.locations.name, data.name),
+      });
 
-    if (existingLocation) {
-      return c.json({ status: "Location Already Exists" }, 409);
+      if (existingLocation) {
+        return c.json({ status: "Location Already Exists" }, 409);
+      }
+
+      await db.insert(schema.locations).values({
+        name: data.name,
+      });
+
+      return c.json({ status: "Location Registered" }, 201);
+    },
+  )
+  .delete("/:name", async (c) => {
+    const name = c.req.param("name");
+    const db = drizzle(c.env.DB, { schema });
+    const deleteCount = await db
+      .delete(schema.locations)
+      .where(eq(schema.locations.name, name))
+      .returning();
+
+    if (deleteCount.length === 0) {
+      return c.json({ status: "Location Not Found" }, 404);
     }
 
-    await db.insert(schema.locations).values({
-      name: data.name,
-    });
-
-    return c.json({ status: "Location Registered" }, 201);
-  },
-);
-
-locations.get("/", async (c) => {
-  const db = drizzle(c.env.DB, { schema });
-  const allLocations = await db.query.locations.findMany();
-  return c.json(allLocations);
-});
-
-locations.delete("/:name", async (c) => {
-  const name = c.req.param("name");
-  const db = drizzle(c.env.DB, { schema });
-  const deleteCount = await db
-    .delete(schema.locations)
-    .where(eq(schema.locations.name, name))
-    .returning();
-
-  if (deleteCount.length === 0) {
-    return c.json({ status: "Location Not Found" }, 404);
-  }
-
-  return c.json({ status: "Location Deleted" });
-});
+    return c.json({ status: "Location Deleted" });
+  });
 
 export default locations;
