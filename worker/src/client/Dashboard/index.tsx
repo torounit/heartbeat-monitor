7e4b53d16884e01419ffb4c653fa1cff41960ab7
torounit <mail@torounit.com>
2026-01-07 02:43:33 +0900

    feat: use suspsense for dashboard

--- worker/src/client/Dashboard/index.tsx
+++ worker/src/client/Dashboard/index.tsx
@@ -1,81 +1,53 @@
-import { hc } from "hono/client";
 import type { InferResponseType } from "hono/client";
-import { useState, useEffect } from "hono/jsx";
+import { hc } from "hono/client";
+import { Suspense, use } from "hono/jsx/dom";
 import type { AppType } from "../../app";
 
-function Status() {
-  const client = hc<AppType>("/");
+const client = hc<AppType>("/");
 
-  const [status, setStatus] =
-    useState<InferResponseType<typeof client.api.status.$get>>();
+async function fetchStatus() {
+  const res = await client.api.status.$get();
+  return res.json();
+}
 
-  const fetchStatus = async () => {
-    try {
-      const res = await client.api.status.$get();
-      if (res.ok) {
-        const data = await res.json();
-        console.log(data);
-        setStatus(data);
-      }
-    } catch (error) {
-      console.error("Failed to fetch data:", error);
-    }
-  };
-
-  useEffect(() => {
-    void fetchStatus();
-  }, []);
-
+function Status({
+  statusPromise,
+}: {
+  statusPromise: Promise<InferResponseType<typeof client.api.status.$get>>;
+}) {
+  const status = use(statusPromise);
   return (
-    <>
-      <h2>Status</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Location</th>
-            <th>Status</th>
-            <th>Last Log At</th>
-            <th>Time Since Last Log (seconds)</th>
+    <table>
+      <thead>
+        <tr>
+          <th>Location</th>
+          <th>Status</th>
+          <th>Last Log At</th>
+          <th>Time Since Last Log (seconds)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {status.map((s) => (
+          <tr key={s.location}>
+            <td>{s.location}</td>
+            <td>{s.status}</td>
+            <td>{new Date(s.lastLogAt).toLocaleString()}</td>
+            <td>{s.timeSinceLastLogSeconds ?? "N/A"}</td>
           </tr>
-        </thead>
-        <tbody>
-          {status?.map((s) => (
-            <tr key={s.location}>
-              <td>{s.location}</td>
-              <td>{s.status}</td>
-              <td>{new Date(s.lastLogAt).toLocaleString()}</td>
-              <td>{s.timeSinceLastLogSeconds ?? "N/A"}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
 function Dashboard() {
-  const fetchStatus = async () => {
-    const client = hc<AppType>("/");
-
-    try {
-      const res = await client.api.status.$get();
-      if (res.ok) {
-        const data = await res.json();
-        console.log(data);
-      }
-    } catch (error) {
-      console.error("Failed to fetch data:", error);
-    }
-  };
-
-  useEffect(() => {
-    void fetchStatus();
-  }, []);
-
   return (
     <>
       <h1>Heartbeat Monitor</h1>
-      <Status />
+      <h2>Status</h2>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Status statusPromise={fetchStatus()} />
+      </Suspense>
     </>
   );
 }
