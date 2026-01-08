ee5b44b280f15e55c9ec2fd5a8f9532b2b98d447
torounit <mail@torounit.com>
2026-01-08 09:28:45 +0900

    refactor: sscheduled

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/d1";
+
 import app from "./app";
 import * as schema from "./db/schema";
 import { sendStatusChangeNotification } from "./services/discord";
@@ -7,19 +8,22 @@ import { updateAllLocationsReports } from "./services/reports";
 const scheduled: ExportedHandlerScheduledHandler<CloudflareBindings> = (
   event,
   env,
-  ctx,
+  ctx
 ) => {
-  ctx.waitUntil(
-    (async () => {
-      const db = drizzle(env.DB, { schema });
-      await updateAllLocationsReports(db, async ({ location, newStatus }) => {
-        const webhookUrl = env.DISCORD_WEBHOOK_URL;
-        if (webhookUrl) {
-          await sendStatusChangeNotification(webhookUrl, location, newStatus);
-        }
-      });
-    })(),
-  );
+  const db = drizzle(env.DB, { schema });
+
+  switch (event.cron) {
+    case "*/1 * * * *":
+      ctx.waitUntil(
+        updateAllLocationsReports(db, async ({ location, newStatus }) => {
+          const webhookUrl = env.DISCORD_WEBHOOK_URL;
+          if (webhookUrl) {
+            await sendStatusChangeNotification(webhookUrl, location, newStatus);
+          }
+        }),
+      );
+      break;
+  }
 };
 
 export default {
