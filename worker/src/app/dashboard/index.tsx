551f3cb917251672e789ea782ae7432f1c1753bf
torounit <mail@torounit.com>
2026-01-09 04:50:19 +0900

    update packages

--- worker/src/app/dashboard/index.tsx
+++ worker/src/app/dashboard/index.tsx
@@ -1,9 +1,7 @@
 import honoFactory from "../../services/honoFactory";
 
-const app = honoFactory
-  .createApp()
-  .get("/", (c) => {
-    return c.render(<div id="root" />);
-  });
+const app = honoFactory.createApp().get("/", (c) => {
+  return c.render(<div id="root" />);
+});
 
 export default app;
