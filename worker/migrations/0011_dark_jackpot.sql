f01e5ba8055551dba1ad5b5ec35603f921c36b84
torounit <mail@torounit.com>
2026-01-12 23:51:10 +0900

    fix migration

--- worker/migrations/0011_dark_jackpot.sql
+++ worker/migrations/0011_dark_jackpot.sql
@@ -1,8 +1,7 @@
-PRAGMA defer_foreign_keys=on;--> statement-breakpoint
-PRAGMA foreign_keys=OFF;--> statement-breakpoint
 ALTER TABLE `locations` RENAME TO `devices`;--> statement-breakpoint
 DROP INDEX `locations_name_unique`;--> statement-breakpoint
 CREATE UNIQUE INDEX `devices_name_unique` ON `devices` (`name`);--> statement-breakpoint
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
 CREATE TABLE `__new_heartbeats` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`device_id` integer NOT NULL,
@@ -10,9 +9,10 @@ CREATE TABLE `__new_heartbeats` (
 	FOREIGN KEY (`device_id`) REFERENCES `devices`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-INSERT INTO `__new_heartbeats`("id", "device_id", "created_at") SELECT "id", "device_id", "created_at" FROM `heartbeats`;--> statement-breakpoint
+INSERT INTO `__new_heartbeats`(`id`, `device_id`, `created_at`) SELECT `id`, `location_id`, `created_at` FROM `heartbeats`;--> statement-breakpoint
 DROP TABLE `heartbeats`;--> statement-breakpoint
 ALTER TABLE `__new_heartbeats` RENAME TO `heartbeats`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
 CREATE TABLE `__new_reports` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`device_id` integer NOT NULL,
@@ -21,8 +21,6 @@ CREATE TABLE `__new_reports` (
 	FOREIGN KEY (`device_id`) REFERENCES `devices`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-INSERT INTO `__new_reports`("id", "device_id", "status", "created_at") SELECT "id", "device_id", "status", "created_at" FROM `reports`;--> statement-breakpoint
+INSERT INTO `__new_reports`(`id`, `device_id`, `status`, `created_at`) SELECT `id`, `location_id`, `status`, `created_at` FROM `reports`;--> statement-breakpoint
 DROP TABLE `reports`;--> statement-breakpoint
-ALTER TABLE `__new_reports` RENAME TO `reports`;--> statement-breakpoint
-PRAGMA foreign_keys=ON;--> statement-breakpoint
-PRAGMA defer_foreign_keys=off;
+ALTER TABLE `__new_reports` RENAME TO `reports`;
