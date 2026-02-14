b8487ecdb9b3d36d0bd8d4ba20b5421b0e79caf6
torounit <mail@torounit.com>
2026-02-14 12:00:47 +0900

    feat: wifi monitor

--- arduino-uno-q/wifi-monitor/sketch/sketch.ino
+++ arduino-uno-q/wifi-monitor/sketch/sketch.ino
@@ -12,29 +12,18 @@ Arduino_LED_Matrix matrix;
 void setup() {
   matrix.begin();
   matrix.clear();
-
   Bridge.begin();
 }
 
 void loop() {
-  String airQuality;
-  bool ok = Bridge.call("get_status").result(airQuality);
+  String status;
+  bool ok = Bridge.call("get_status").result(status);
   if (ok) {
-    if (airQuality == "Good") {
+    if (status == "Good") {
       matrix.loadFrame(good);
-    } else if (airQuality == "Moderate") {
-      matrix.loadFrame(moderate);
-    } else if (airQuality == "Unhealthy for Sensitive Groups") {
-      matrix.loadFrame(unhealthy_for_sensitive_groups);
-    } else if (airQuality == "Unhealthy") {
-      matrix.loadFrame(unhealthy);
-    } else if (airQuality == "Very Unhealthy") {
-      matrix.loadFrame(very_unhealthy);
-    } else if (airQuality == "Hazardous") {
-      matrix.loadFrame(hazardous);
     } else {
-      matrix.loadFrame(unknown);
+      matrix.loadFrame(unhealthy);
     }
   }
-  delay(1000);
+  delay(30000);
 }
